11ab9a5c6f553b636532b204e18799bd5fe117ac
baibing <baibing@rongcloud.cn>
2020-02-03 00:14:58 +0800

    add cases.

--- src/main/java/com/walle/leetcode/LeetCode26.java
+++ src/main/java/com/walle/leetcode/LeetCode26.java
@@ -1,10 +1,11 @@
 package com.walle.leetcode;
 
-public class Array1 {
+//leetcode 26
+public class LeetCode26 {
     public int removeDuplicates(int[] nums) {
         if (nums.length <= 1)
             return nums.length;
-        int i = 0,j = 0;
+        int i = 0, j = 0;
         while (j < nums.length) {
             nums[i] = nums[j];
 
@@ -28,9 +29,9 @@ public class Array1 {
     }
 
     public static void main(String[] args) {
-        int[] list = {0,0,1,1,1,2,2,3,3,4};
+        int[] list = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
 
-        Array1 array = new Array1();
+        LeetCode26 array = new LeetCode26();
         int len = array.removeDuplicates(list);
         for (int i = 0; i < len; i++) {
             System.out.print(list[i] + " ");
---
+++ src/main/java/com/walle/leetcode/Leetcode26_2.java
@@ -0,0 +1,30 @@
+package com.walle.leetcode;
+
+//leetcode 26
+public class Leetcode26_2 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int i = 0, j = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                ++j;
+            } else {
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1,2,2};
+
+        Leetcode26_2 array = new Leetcode26_2();
+        int len = array.removeDuplicates(list);
+        for (int i = 0; i < len; i++) {
+            System.out.print(list[i] + " ");
+        }
+
+    }
+}
