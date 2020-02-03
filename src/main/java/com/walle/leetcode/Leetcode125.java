ece6041810b6e5afd37df85f09c6e8acda3fe6b8
baibing <baibing@rongcloud.cn>
2020-02-03 15:24:14 +0800

    add cases.

---
+++ src/main/java/com/walle/leetcode/Leetcode125.java
@@ -0,0 +1,40 @@
+package com.walle.leetcode;
+
+/*
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+ */
+public class Leetcode125 {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (!isValid(s.charAt(i))) {
+                ++i;
+            }
+            else if (!isValid(s.charAt(j))) {
+                --j;
+            } else {
+                if (s.charAt(i) != s.charAt(j))
+                    return false;
+                else {
+                    ++i;
+                    --j;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char ch) {
+        return ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z')) ;
+    }
+
+    public static void main(String[] args) {
+        Leetcode125 lc = new Leetcode125();
+        String input = "A man, a plan, a canal: Panama";
+        System.out.println(lc.isPalindrome(input));
+    }
+}
---
+++ src/main/java/com/walle/leetcode/Leetcode136.java
@@ -0,0 +1,27 @@
+package com.walle.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+    给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+    说明：
+
+    你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ */
+public class Leetcode136 {
+    public int singleNumber(int[] nums) {
+        int sum = 0;
+        for (int i : nums) {
+            sum ^= i;// 用 异或， 两个相同的数异或为0，最后剩下来是 单个的
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Leetcode136 lc = new Leetcode136();
+        int[] input = {4, 1, 2, 1, 2};
+        System.out.println(lc.singleNumber(input));
+    }
+}
