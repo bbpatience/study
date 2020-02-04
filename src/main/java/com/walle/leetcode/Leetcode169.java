5e57836715ce0fafba06c1187252bd4450562579
baibing <baibing@rongcloud.cn>
2020-02-04 23:49:03 +0800

    add cases.

--- src/main/java/com/walle/leetcode/Leetcode169.java
+++ src/main/java/com/walle/leetcode/Leetcode169.java
@@ -2,16 +2,23 @@ package com.walle.leetcode;
 
 import java.util.Arrays;
 
-public class MajorityInArray {
+/*
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ */
+
+public class Leetcode169 {
+    // 投票算法
     public static int majorityElement(int[] nums) {
         int count = 0;
-        Integer candidate = null;
+        int candidate = Integer.MIN_VALUE;
 
         for (int num : nums) {
             if (count == 0) {
                 candidate = num;
             }
-            count += (num == candidate) ? 1 : -1;
+            count += (num == candidate) ? 1 : -1; //同则+1，不同则-1. 因为存在大于n/2的数，最终candidate会变为candidate。
         }
 
         return candidate;
@@ -24,7 +31,7 @@ public class MajorityInArray {
         int candidateCnt = 1;
         int i = 1;
         while (i < nums.length) {
-            if (nums[i] == nums[i-1]) {
+            if (nums[i] == nums[i - 1]) {
                 count++;
             } else {
                 if (count > candidateCnt) {
@@ -40,8 +47,14 @@ public class MajorityInArray {
         }
         return candidate;
     }
+
+    public static int majorityElement3(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
     public static void main(String[] args) {
         int[] nums = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 7, 7, 7, 7};
-        System.out.println(majorityElement2(nums));
+        System.out.println(majorityElement3(nums));
     }
 }
---
+++ src/main/java/com/walle/leetcode/Leetcode190.java
@@ -0,0 +1,23 @@
+package com.walle.leetcode;
+
+/*
+颠倒给定的 32 位无符号整数的二进制位。
+ */
+
+public class Leetcode190 {
+    public static int reverseBits(int n) {
+        int sum = 0;
+        for (int i = 0; i < 32; ++i) {
+            int mask = 1 << (31 - i);
+            if ((n & mask) == mask)
+                sum |= (1 << i);
+//            System.out.println(Integer.toString(sum,2) );
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int test = 43261596;
+        System.out.println(reverseBits(test));
+    }
+}
