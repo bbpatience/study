9a61d06339bb85c30da06c439e35af1dc85b4f1d
baibing <baibing@rongcloud.cn>
2020-02-05 18:56:27 +0800

    add cases.

--- src/main/java/com/walle/leetcode/Leetcode219.java
+++ src/main/java/com/walle/leetcode/Leetcode219.java
@@ -12,15 +12,10 @@ public class Leetcode219 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {
-            if (map.containsKey(nums[i])) {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                } else {
-                    map.put(nums[i], i);
-                }
-            } else {
-                map.put(nums[i], i);
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
             }
+            map.put(nums[i], i);
         }
         return false;
     }
