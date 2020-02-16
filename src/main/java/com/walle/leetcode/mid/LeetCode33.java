ce688a7b76a3f717160b2ad24337a80f0fb13579
baibing <baibing@rongcloud.cn>
2020-02-16 16:18:43 +0800

    add cases.

---
+++ src/main/java/com/walle/leetcode/mid/LeetCode33.java
@@ -0,0 +1,74 @@
+package com.walle.leetcode.mid;
+
+/*
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+
+你可以假设数组中不存在重复的元素。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+ */
+public class LeetCode33 {
+
+    /**
+     * 二分法，O(log n),  找到发生变化的idx， 两次二分法
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        if (nums.length <= 0)
+            return -1;
+        if (nums.length == 1)
+            return nums[0] == target ? 0 : -1;
+        int idx = findIdx(nums, 0, nums.length - 1);
+        if (idx < 0) {
+            return search(nums, 0, nums.length - 1, target);
+        }
+        return Math.max(search(nums, 0, idx, target), search(nums, idx + 1, nums.length - 1, target));
+    }
+
+    private int search(int[] a, int start, int end, int value) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] > value) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int findIdx(int[] nums, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[start] <= nums[mid] && nums[mid + 1] <= nums[end]) {
+                return mid;
+            } else if (nums[start] > nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        LeetCode33 lc = new LeetCode33();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+//        int[] nums = {0, 1, 2, 3, 4, 5, 6};
+//        int[] nums = {3, 5, 1};
+        for (int i : nums) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
+        System.out.println(lc.search(nums, 0));
+    }
+}
---
+++ src/main/java/com/walle/leetcode/mid/LeetCode39.java
@@ -0,0 +1,48 @@
+package com.walle.leetcode.mid;
+
+import java.util.*;
+
+/*
+给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的数字可以无限制重复被选取。
+
+说明：
+
+所有数字（包括 target）都是正整数。
+解集不能包含重复的组合。 
+ */
+public class LeetCode39 {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Stack<Integer> stack = new Stack<>();
+        List<List<Integer>> resultList = new ArrayList<>();
+        search(candidates, target, stack, resultList, 0);
+        return resultList;
+    }
+
+    public void search(int[] candidates, int target, Stack<Integer> stack, List<List<Integer>> resultList, int begin) {
+        if (target < 0) {
+            return;
+        } else if (target == 0) {
+            resultList.add((List) stack.clone());
+            return;
+        }
+        for (int i = begin; i < candidates.length; ++i) {
+            stack.push(candidates[i]);
+            search(candidates, target - candidates[i], stack, resultList, i);
+            stack.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        LeetCode39 lc = new LeetCode39();
+        int[] nums = {2, 3, 6, 7};
+        int target = 7;
+
+        lc.combinationSum(nums, target).forEach(list -> {
+            list.forEach(item -> System.out.print(item + ","));
+            System.out.println();
+        });
+    }
+}
