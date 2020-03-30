3665b20c593aa4d099f281d500cf3778476d0c82
baibing <baibing@rongcloud.cn>
2020-03-30 14:01:52 +0800

    add case.

---
+++ src/main/java/com/walle/leetcode/simple/Leetcode62.java
@@ -0,0 +1,31 @@
+package com.walle.leetcode.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+ * <p>
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ */
+public class Leetcode62 {
+
+    public int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while (n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
+    }
+
+    public static void main(String[] args) {
+        Leetcode62 lc = new Leetcode62();
+        System.out.println(lc.lastRemaining(5, 3));
+    }
+}
